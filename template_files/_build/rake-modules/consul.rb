fe537394f8b0c6c97172eb5b22e9ae88350e58dc
Nic Jackson <jackson.nic@gmail.com>
2016-01-28 19:47:10 +0000

    added minke gem to replace build steps

--- template_files/_build/rake-modules/consul.rb
+++
@@ -1,7 +0,0 @@
-def setConsulVariables host, port
-  puts "Setting Consul key values for server: #{host}:#{port}"
-
-  kvs = Consul::Client::KeyValue.new :api_host => host, :api_port => port, :logger => Logger.new("/dev/null")
-
-  kvs.put('StatsDServerIP','statsd:8125')
-end
